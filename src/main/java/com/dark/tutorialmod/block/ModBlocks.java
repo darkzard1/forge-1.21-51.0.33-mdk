5bf87217743a14bd894d0ef932c7c3d22173d27f
dcarr <dcarroll4799@gmail.com>
2024-11-12 20:20:31 -0500

    yo

---
+++ src/main/java/com/dark/tutorialmod/block/ModBlocks.java
@@ -0,0 +1,46 @@
+package com.dark.tutorialmod.block;
+
+import com.dark.tutorialmod.TutorialMod;
+import com.dark.tutorialmod.items.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<Block> CYRENIAN_ORE =  registerBlock("cyrenian_ore",
+            () -> new Block(BlockBehaviour.Properties.of()
+                    .strength(4f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
+
+    public static final RegistryObject<Block> RAW_CYRENIAN_BLOCK =  registerBlock("raw_cyrenian_block",
+            () -> new Block(BlockBehaviour.Properties.of()
+                    .strength(3f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
+
+    public static final RegistryObject<Block> CYRENIAN_BLOCK =  registerBlock("cyrenian_block",
+            () -> new Block(BlockBehaviour.Properties.of()
+                    .strength(4f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
---
+++ src/main/java/com/dark/tutorialmod/items/ModItems.java
@@ -0,0 +1,26 @@
+package com.dark.tutorialmod.items;
+
+import com.dark.tutorialmod.TutorialMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+        DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<Item> RAW_CYRENIAN = ITEMS.register("raw_cyrenian",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> CYRENIAN_NUGGET = ITEMS.register("cyrenian_nugget",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> CYRENIAN_INGOT = ITEMS.register("cyrenian_ingot",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
